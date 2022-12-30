6b2c0a9a0cb258cdd815af8ef1f9fddd10be3623
Derek Van Ittersum <derekvan@memoryfailure.net>
2022-12-30 18:47:17 -0500

    working functionality

--- main.ts
+++ main.ts
@@ -1,137 +1,167 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, SuggestModal } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+function getHeadings(): any[] {
+	const { metadataCache } = this.app
+	const workspace = this. app.workspace
+	const fileTitle = workspace.getActiveFile();
+	let headings: any[]
+	if (fileTitle == null) {
+		return[];
+	} else {
+		headings = metadataCache.getFileCache(fileTitle)?.headings;
+		return headings
+	}
+// Get the headers and prompt user to select
+	
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function getHeaders(): string[]{
+	const headings = getHeadings()
+	const headers: any[] = []
+	if (!headings) return [];
+		headings.forEach(hh => {
+		headers.push(hh.heading)
+	});
+
+	return headers
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MoveHeader extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'move-to-heading',
+			name: 'Move text to heading',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				new HeaderModal(this.app, (value) => {
+				}).open();
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
+}
+			
+				
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+class HeaderModal extends SuggestModal<string> {
+    app: App;
+	cb: (value:string) => void;
+
+    constructor(app: App,cb: (value:string)=>void){
+        super(app);
+        this.app = app;
+		this.cb = cb;
+    }
+
+
+		// Returns all available suggestions.
+		getSuggestions(query: string) {
+		const headers = getHeaders()
+		return headers.filter((h: string) =>
+			h.toLowerCase().includes(query.toLowerCase())
+			);
+		}
+
+		// Renders each suggestion item.
+		renderSuggestion(header: string, el: HTMLElement) {
+			el.createEl("div", { text: header });
+		}
+
+		// Perform action on the selected suggestion.
+		onChooseSuggestion(header: string, evt: MouseEvent | KeyboardEvent) {
+			this.cb(header)
+			new HeaderSecondModal(this.app, (choice) => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (!view) return []
+				const editor = view.editor
+				const doc = view.getViewData();
+				const headings = getHeadings();
+				let content = "";
+				const isSelection = editor.somethingSelected();
+				const selection = editor.getSelection();
+				const line = editor.getLine(editor.getCursor().line);
+
+				// get the selection or line
+
+				if (isSelection) {
+					content = selection;
+				} 
+				else {
+					content = line;
 				}
+				let position: string | any[] = []
+				let existing = ""
+				let isLast = false;
+
+				// Get the existing content of the heading before adding the selected line 
+
+				for (let i = 0; i < headings.length; i++) { 
+					const heading = headings[i];
+					if (heading.heading == header) { 
+						if (i == headings.length - 1) { 
+							position = [heading.position.start.line, -1];
+						} 
+						else { 
+							position = [heading.position.start.line, headings[i + 1].position.start.line]; 
+						} 
+						if (heading.position.start.line == editor.lastLine()){
+							isLast = true
+							console.log("isLast is true")
+						}
+					} 
+				} 
+				if (position) {
+					let newText = ""
+					const oldText = doc.replace(content,"");
+					if (isLast == false){ 
+						existing = doc.split("\n").slice(position[0], position[1]).join("\n");
+						const matchHeader = existing.match(/#.*/)
+						if (!matchHeader) return[]
+						const matchBody = existing.replace(/#.*/,"").trimStart();			
+							if(choice == "prepend"){
+								newText	= oldText.replace(existing,matchHeader[0] + "\n\n" + content + "\n" + matchBody);
+							}
+							
+							if(choice == "append"){
+								console.log(`existing = ${existing} and content = ${content}`)
+								newText = oldText.replace(existing,existing + "\n" + content);
+							}
+						view.setViewData(newText, false)
+					}
+					else {
+						view.setViewData(oldText + "\n\n" + content,false)
+					}			
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+			}).open();
+		}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class HeaderSecondModal extends SuggestModal<string> {
+    app: App;
+	cb2: (choice:string) => void;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App,cb2: (choice:string)=>void){
+        super(app);
+        this.app = app;
+		this.cb2 = cb2;
+    }
+		// Returns all available suggestions.
+		getSuggestions(query: string) {
+		const choices = ["prepend","append"]
+		return choices.filter((c: string) =>
+			c.toLowerCase().includes(query.toLowerCase())
+			);
+		}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		// Renders each suggestion item.
+		renderSuggestion(c: string, el: HTMLElement) {
+			el.createEl("div", { text: c });
+		}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		// Perform action on the selected suggestion.
+		onChooseSuggestion(c: string, evt: MouseEvent | KeyboardEvent) {
+			this.cb2(c)
+		}
 }
